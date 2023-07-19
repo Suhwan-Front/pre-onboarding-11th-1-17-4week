fe7c52ede1421cbd5aaf9309cb4db129944c8cb7
Suhwan0818 <tnghks1997@gmail.com>
2023-07-19 14:42:05 +0900

    ✅ 검색어 없음까지 구현

--- src/components/InputBox/InputBox.tsx
+++ src/components/InputBox/InputBox.tsx
@@ -2,11 +2,16 @@ import React, { useContext, useState } from 'react'
 import { InputBoxWrapper } from './InputBoxPresenter'
 import { HealthListContext } from 'contexts/HealthListContext'
 import { type HealthData } from 'types/types'
+import {
+  MAX_RECENT_SEARCHES,
+  MAX_RELATED_SEARCHES,
+} from 'components/constants/const'
 
 const InputBox: React.FC = () => {
   const { healthList } = useContext(HealthListContext)
   const [searchQuery, setSearchQuery] = useState<string>('')
-  const [relatedSearches, setRelatedSearched] = useState<HealthData[]>([])
+  const [relatedSearches, setRelatedSearches] = useState<HealthData[]>([])
+  const [recentSearches, setRecentSearches] = useState<string[]>([])
 
   const handleInputBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
@@ -14,7 +19,19 @@ const InputBox: React.FC = () => {
     const filteredData = healthList.filter((sick) =>
       sick.sickNm.includes(value),
     )
-    setRelatedSearched(filteredData)
+    setRelatedSearches(filteredData.slice(0, MAX_RELATED_SEARCHES))
+  }
+
+  const handleInputBoxClick = () => {
+    setRecentSearches((prevSearches) => [
+      searchQuery,
+      ...prevSearches.slice(0, MAX_RECENT_SEARCHES),
+    ])
+  }
+
+  const handleInputBoxBlur = () => {
+    setRecentSearches([])
+    setRelatedSearches([])
   }
 
   return (
@@ -24,14 +41,28 @@ const InputBox: React.FC = () => {
         value={searchQuery}
         onChange={handleInputBoxChange}
         placeholder="검색어를 입력하세요"
+        onClick={handleInputBoxClick}
+        onBlur={handleInputBoxBlur}
       />
       <button>검색</button>
-      {relatedSearches.length > 0 && (
-        <ul>
+      {relatedSearches.length === 0 ? (
+        <>
+          <div>최근 검색어</div>
+          {recentSearches.length > 0 ? (
+            recentSearches.map((search, index) => <li key={index}>{search}</li>)
+          ) : (
+            <li>검색어 없음</li>
+          )}
+          <div>추천 검색어</div>
+          <li>검색어 없음</li>
+        </>
+      ) : (
+        <>
+          <div>추천 검색어</div>
           {relatedSearches.map((sick) => (
             <li key={sick.sickCd}>{sick.sickNm}</li>
           ))}
-        </ul>
+        </>
       )}
     </InputBoxWrapper>
   )
